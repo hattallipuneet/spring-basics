8e00f3bfbcbd556b34c1a4876273f2d62ffecd38
hattallipuneet <hattallipuneet@gmail.com>
2019-08-04 12:45:17 +0530

    Update package-info.java

--- src/main/java/spring/javaconfig/injectimportbeans/package-info.java
+++ src/main/java/spring/javaconfig/injectimportbeans/package-info.java
@@ -4,6 +4,11 @@
  *
  * The simple technique used here is to define these dependencies as parameters of {@link org.springframework.context.annotation.Bean}
  * annotated factory method.
- * 
+ *
+ * Note: [CAUTIONARY WARNING]
+ *      Another technique used for injecting imported beans is to use the @Autowire annotation to
+ *      inject the dependency into the config class. Then this dependency bean can used to in @Bean
+ *      annotated methods that create desired bean.
+ *
  * */
 package spring.javaconfig.injectimportbeans;
