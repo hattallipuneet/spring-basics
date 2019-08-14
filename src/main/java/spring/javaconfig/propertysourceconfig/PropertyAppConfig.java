8e4e4324744f019a177a19bbb028e7ec14122860
hattallipuneet <hattallipuneet@gmail.com>
2019-08-15 00:20:32 +0530

    Inject property from PropertySource

--- src/main/java/spring/javaconfig/propertysourceconfig/PropertyAppConfig.java
+++ src/main/java/spring/javaconfig/propertysourceconfig/PropertyAppConfig.java
@@ -1,6 +1,7 @@
 package spring.javaconfig.propertysourceconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,8 +11,22 @@ import org.springframework.core.env.Environment;
 @PropertySource("classpath:properties/app-config.properties")
 public class PropertyAppConfig {
 
+    @Value("${property_1}")
+    private String property;
+
+    @Value("${property_2:default}")
+    private String propertyWithDefault;
+
     @Autowired
-    Environment environment;
+    private Environment environment;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public String getPropertyWithDefault() {
+        return propertyWithDefault;
+    }
 
     @Bean
     public TestBean testBean() {
--- src/test/java/spring/javaconfig/propertysourceconfig/PropertySourceConfigTest.java
+++ src/test/java/spring/javaconfig/propertysourceconfig/PropertySourceConfigTest.java
@@ -12,11 +12,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class PropertySourceConfigTest {
 
     @Autowired
+    private PropertyAppConfig appConfig;
+
+    @Autowired
     private TestBean testBean;
 
     @Test
     public void testPropertySource() {
         Assert.assertNotNull(testBean);
         Assert.assertEquals("testBean", testBean.getBeanName());
+    }
+
+    @Test
+    public void testPropertyInjectionFromPropertySource() {
+        Assert.assertNotNull(appConfig.getProperty());
+        Assert.assertEquals("from_properties_file", appConfig.getProperty());
+
+        Assert.assertNotNull(appConfig.getPropertyWithDefault());
+        Assert.assertEquals("default", appConfig.getPropertyWithDefault());
     }
 }
